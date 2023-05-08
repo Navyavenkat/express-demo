06cac78fe1dda19bf8422a234d95cb1f6a6720ad
navyavenkat <navyavenkat03@gmail.com>
2023-05-07 22:55:34 -0700

    Adding the list of  arrays by using params

--- service.js
+++ service.js
@@ -1,9 +1,36 @@
 const express=require('express')
-const serviceRouter=express.Router()
-
-serviceRouter.get('/',(request,response)=>
-{
-    response.send("this is our serrvices")
+const router=express.Router()
+router.get('/',(req,res)=>{
+  res.send("Service Page")
 })
 
-module.exports=serviceRouter
+router.get('/:id',(req,res)=>{
+const pageId=Number(req.params.id);
+  const serviceId=service.find((ser)=>ser.id===pageId);
+  if(!serviceId){
+    res.send("Service Not Found")
+  }
+  else{
+    res.json(service[pageId])
+  }
+})
+const service=[
+  {
+    id:0,
+    name:'App development'
+  },
+  {
+    id:1,
+    name:'Web development'
+  },
+  {
+    id:2,
+    name:'Iot development'
+  },
+  {
+    id:3,
+    name:'Embedded system'
+
+  }
+]
+module.exports=router;
--- users.js
+++ users.js
@@ -1,13 +1,10 @@
 const express=require('express')
 const usersRouter=express.Router()
 
+
 usersRouter.get('/',(request,response)=>{
     response.send("this is userspage")
 })
-
-
-
-
 
 
 module.exports=usersRouter
